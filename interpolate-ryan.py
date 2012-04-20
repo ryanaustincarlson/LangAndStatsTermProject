4da28ea3c6fee744d61f905899f2857fddb7d74d
Ryan Carlson <rcarlso1@gmail.com>
2012-04-20 14:25:19 -0400

    added ryan's interpolation script and a script to write out model predictions given a dev set, one word per line

---
+++ interpolate-ryan.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+import random, math
+
+def read_probs_file(fname):
+  return [float(line.strip()) for line in open(fname, 'r')]
+
+def randomly_generate_weights(num_weights):
+  weights = []
+  for i in xrange(num_weights):
+    while True:
+      rand = random.uniform(0,1)
+      if rand > 0 and rand < 1:
+        break
+
+    weights.append(rand)
+
+  weights_sum = math.fsum(weights)
+
+  for i in xrange(len(weights)):
+    weights[i] /= weights_sum
+
+  return weights
+
+def run_algorithm_until_convergence(probability_models, initial_weights, convergence_num):
+
+  num_types = len(probability_models[0])
+
+  # translate all numbers into equation notation so its easier to check
+  # that the equation is right
+  N = num_types
+  K = len(initial_weights)
+  P = probability_models
+
+  def update(weights):
+    l = weights # l := lambda
+
+    new_weights = []
+    for j in xrange(K):
+
+      N_sum = 0
+      for i in xrange(N):
+        numerator = l[j] * P[j][i]
+
+        denominator = 0
+        for k in xrange(K):
+          denominator += l[k] * P[k][i]
+          #print l[k], P[k][i]
+        N_sum += (float(numerator) / denominator)
+      new_weights.append( 1.0 / N * N_sum )
+
+    return new_weights
+  
+  def check_convergence(old_weights, new_weights):
+    def log_likelihood(weights):
+      l = weights # l := lambda
+      N_sum = 0
+
+      for i in xrange(N):
+        K_sum = 0
+        for k in xrange(K):
+          K_sum += l[k] * P[k][i]
+
+        N_sum += math.log(K_sum)
+      return float(N_sum) / N
+    
+    old_likelihood = log_likelihood(old_weights)
+    new_likelihood = log_likelihood(new_weights)
+
+    percent_change = float(new_likelihood - old_likelihood) / math.fabs(new_likelihood)
+
+    return (
+        percent_change <= convergence_num, 
+        new_likelihood, 
+        float(new_likelihood) / old_likelihood
+        )
+
+  weights = initial_weights
+  print "Initial Weights:", [round(weight, 6) for weight in weights], '\n'
+  iteration = 1
+  while True:
+    new_weights = update(weights)
+    print "Iteration %d" % iteration
+    print "\tWeights:", [round(weight, 6) for weight in new_weights]
+
+    convergence_stats = check_convergence(weights, new_weights)
+    has_converged = convergence_stats[0]
+    average_likelihood = convergence_stats[1]
+    ratio = convergence_stats[2]
+
+    print "\tAverage Log-Likelihood:", average_likelihood
+    print "\tLog-Likelihood Ratio:", ratio
+
+    if has_converged: break
+    weights = new_weights
+    iteration += 1
+    print
+
+def count_zeros(probabilities):
+  print 'zero counts:', [sum((1 for prob in prob_model if prob == 0)) for prob_model in probabilities]
+
+def main():
+  import sys
+
+  if len(sys.argv) == 1:
+    print 'usage: %s <prob-files>' % sys.argv[0]
+    print '       prob-files contain lists of probabilities generated from the same dev set (and end in .prob)'
+    sys.exit(1)
+
+  fnames = sys.argv[1:]
+  probabilities = [read_probs_file(fname) for fname in fnames]
+
+  #probabilities = [probabilities[0], probabilities[1], probabilities[0]]
+
+  uniform_model = [1.0/4000]*len(probabilities[0])
+  probabilities.append(uniform_model)
+
+  #count_zeros(probabilities)
+
+  weights = randomly_generate_weights(len(probabilities))
+  convergence_num = 0.001
+
+  run_algorithm_until_convergence(probabilities, weights, convergence_num)
+
+
+if __name__ == '__main__':
+  main()
+
---
+++ write_model_predictions.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import sys, logging
+
+# models
+from Unigram import Unigram
+from Bigram import Bigram 
+from Trigram import Trigram
+
+# turn logging on or off
+LOGGING_LEVEL = logging.DEBUG; #LOGGING_LEVEL = None
+
+# want the format of output to be
+#   output_dir/dev_filename-model_name.probs
+def get_output_filename(output_dir, dev_filename, model_name):
+  file_ext_index = dev_filename.find('.'); directory_index = dev_filename.rfind('/')
+  if file_ext_index > 0: dev_filename = dev_filename[:file_ext_index]
+  if directory_index > 0: dev_filename = dev_filename[directory_index+1:]
+  if not output_dir.endswith('/'): output_dir += '/'
+
+  return output_dir + dev_filename + '-' + model_name + '.probs'
+
+def main():
+  logging.basicConfig(level=LOGGING_LEVEL, format="DEBUG: %(message)s")
+
+  if len(sys.argv) < or len(sys.argv) > 4:
+    print 'usage: %s training-file dev-file [output-dir]' % sys.argv[0]
+    print '       output-dir is optional, default is "output"'
+    sys.exit(1)
+
+  training_filename = sys.argv[1]
+  dev_filename = sys.argv[2]
+  try:
+    output_dir = sys.argv[3] 
+  else:
+    output_dir = 'output'
+
+  logging.debug('Training models...')
+
+  # train all the models!
+  unigram_model = Unigram(training_filename)
+  logging.debug('Done training unigram model')
+  bigram_model = Bigram(training_filename)
+  logging.debug('Done training bigram model')
+  trigram_model = Trigram(training_filename)
+  logging.debug('Done training trigram model')
+
+  dev_words = [line.strip() for line in open(dev_filename, 'r')]
+
+  # write predictions out to disk
+  unigram_model.write_probability_list(dev_words, get_output_filename(output_dir, dev_filename, 'unigram'))
+  logging.debug('Wrote dev set predictions using unigram model')
+  bigram_model.write_probability_list(dev_words, get_output_filename(output_dir, dev_filename, 'bigram'))
+  logging.debug('Wrote dev set predictions using bigram model')
+  trigram_model.write_probability_list(dev_words, get_output_filename(output_dir, dev_filename, 'trigram'))
+  logging.debug('Wrote dev set predictions using trigram model')
+
+if __name__ == '__main__': main()
