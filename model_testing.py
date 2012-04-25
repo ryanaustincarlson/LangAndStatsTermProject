35d97f1076a206f8ec53ca7d13ce22251f9e6a24
Peter Schulam <pschulam@cs.cmu.edu>
2012-04-25 12:12:34 -0400

    Update model_testing.py (code for testing our model)
    This code still isn't usable, though. The load_model() function
    doesn't do anything and so the whole process will fail. We need
    to decide on a protocol for loading our super model into memory.

--- model_testing.py
+++ model_testing.py
@@ -3,16 +3,16 @@
 """
 import math
 
+VOCAB_FILE = 'data/vocabulary.txt'
+
 def next_tag_probs(model, history, vocabulary):
     """Returns a list of probabilities for each word in the
     vocabulary given the model and the history.
 
     """
-    tag_probs = []
-    for v in vocabulary:
-        p = model.get_probability(v, history)
-        tag_probs.append(p)
-    return tag_probs
+    tag_probs = [model.get_probability(v, history)
+                 for v in vocabulary]
+    return zip(tag_probs, vocabulary)
 
 def perplexity(token_probs):
     """Returns the perplexity of list of probabilities
@@ -24,7 +24,24 @@ def perplexity(token_probs):
     return 2 ** (-avg_ll)
 
 def load_model(model_path):
+    """Load our big interpolated model from disk. I'm guessing
+    we should have an InterpolatedModel class and just assume
+    that this is the only model we'll ever want to load.
+
+    """
     pass
+
+def predict(model, history, vocabulary):
+    """Pass the model, history, and vocabulary to this function.
+    Returns a 2-tuple in which the most likely token is the first
+    item, and a dictionary mapping tokens to likelihoods is the
+    second item.
+
+    """
+    tag_probs = next_tag_probs(model, history, vocabulary)
+    prediction = max(tag_probs)[1]
+    tag_probs = dict(v, p for p, v in tag_probs)
+    return prediction, tag_probs
 
 if __name__ == '__main__':
     import sys
@@ -34,5 +51,23 @@ if __name__ == '__main__':
         sys.exit(1)
 
     m = load_model(sys.argv[1])
-    for line in sys.stdin:
+    vocabulary = [l.strip() for l in open(VOCAB_FILE)]
 
+    history = []
+    token_probs = []
+    missed_predictions = 0
+
+    for line in sys.stdin:
+        prediction, tag_probs = predict(m, history, vocabulary)
+        print ' '.join(tag_probs[v] for v in vocabulary)
+        sys.stdout.flush()
+        token = line.strip()
+
+        if not token == prediction: missed_predictions += 1
+        history.append(token)
+        token_probs.append(tag_probs[token])
+
+    print '{0} {1}'.format(float(missed_prediction) / len(history),
+                           perplexity(token_probs))
+    sys.stdout.flush()
+        
