a1215b5f5e2a275c87f0a01b6e622eec763e00d6
Peter Schulam <pschulam@cs.cmu.edu>
2012-04-28 15:49:23 -0400

    Remove PyYAML, NLTK from compile.sh, replace with SRILM

--- compile.sh
+++ compile.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-PYYAML_URL='http://pyyaml.org/download/pyyaml/PyYAML-3.09.tar.gz'
-NLTK_URL='http://nltk.googlecode.com/files/nltk-2.0.1rc1.tar.gz'
 MAXENT_URL='git://github.com/lzhang10/maxent.git'
+SRILM_TARBALL='./resources/srilm.tar.gz'
 
 BUILD_DIR=build
-PYYAML_DIR=$(basename $PYYAML_URL | perl -npe 's/\.tar\.gz//')
-NLTK_DIR=$(basename $NLTK_URL | perl -npe 's/\.tar\.gz//')
 MAXENT_DIR=$(basename $MAXENT_URL | perl -npe 's/\.git//')
+SRILM_DIR=srilm
 
 [ -d $BUILD_DIR ] || mkdir $BUILD_DIR
-
-# Install PyYaml
-( 
-cd $BUILD_DIR; 
-wget $PYYAML_URL; 
-tar xzf `basename $PYYAML_URL`;
-cd $PYYAML_DIR; 
-python setup.py install --user;
-)
-
-# Install NLTK
-(
-cd $BUILD_DIR; 
-wget $NLTK_URL;
-tar xzf `basename $NLTK_URL`;
-cd $NLTK_DIR;
-python setup.py install --user;
-)
 
 # Install maxent
 (
@@ -39,4 +19,15 @@ make;
 cd python;
 python setup.py build;
 python setup.py install --user;
+)
+
+# Install SRILM
+(
+cd $BUILD_DIR
+mkdir $SRILM_DIR
+cd $SRILM_DIR
+tar xzf $SRILM_TARBALL
+MT=`./sbin/machine-type`
+SRILM=`pwd -P`
+make MACHINE_TYPE=$MT SRILM=$SRILM NO_TCL=X
 )
