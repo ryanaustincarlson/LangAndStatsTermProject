50a3249b81d0a55da025083a6bff248578cc9a4f
Peter Schulam <pschulam@cs.cmu.edu>
2012-04-28 16:20:53 -0400

    Fix TCL_INCLUDE bug

--- compile.sh
+++ compile.sh
@@ -29,5 +29,5 @@ cd $SRILM_DIR
 tar xzf $SRILM_TARBALL
 MT=`./sbin/machine-type`
 SRILM=`pwd -P`
-make MACHINE_TYPE=$MT SRILM=$SRILM TCL_INCLUDE=/usr/ TCL_LIBRARY=/usr/lib64/libtcl.so
+make MACHINE_TYPE=$MT SRILM=$SRILM TCL_INCLUDE=/usr/tcl.h TCL_LIBRARY=/usr/lib64/libtcl.so
 )
